6d7d5d9b0f02e14cee20c9ef7a02c4afbea4b99f
svjunic <sv.junic@gmail.com>
2026-01-06 12:39:25 +0900

    style(telescope): 境界の背景をNONEに変更
    Telescope の各 Border ハイライトの背景を p.bg0 から "NONE" に変更
    しました。これにより透明化され、termguicolors 有効時にウィンドウと
    視覚的に馴染むようになります。

--- lua/radicalgoodspeed/integrations/telescope.lua
+++ lua/radicalgoodspeed/integrations/telescope.lua
@@ -7,10 +7,10 @@ end
 
 function M.get(p)
   return {
-    TelescopeBorder = { fg = p.fg3, bg = p.bg0 },
-    TelescopePromptBorder = { fg = p.fg3, bg = p.bg0 },
-    TelescopeResultsBorder = { fg = p.fg3, bg = p.bg0 },
-    TelescopePreviewBorder = { fg = p.fg3, bg = p.bg0 },
+    TelescopeBorder = { fg = p.fg3, bg = "NONE" },
+    TelescopePromptBorder = { fg = p.fg3, bg = "NONE" },
+    TelescopeResultsBorder = { fg = p.fg3, bg = "NONE" },
+    TelescopePreviewBorder = { fg = p.fg3, bg = "NONE" },
 
     TelescopeSelection = { bg = p.bg2 },
     TelescopeMatching = { fg = p.orange0, bold = true },
